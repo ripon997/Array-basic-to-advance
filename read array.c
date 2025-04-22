f98b25d22a9b4ad143c046d01eac8ae6d7409787
Ripon <ripon365cse@gmail.com>
2025-04-22 21:23:54 +0600

    read a integer array

---
+++ read array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int numbers[]={10,20,30,40,50};
+    int i;
+    for(i=0;i<5;i++){
+        printf("%d number index value %d\n",i,numbers[i]);
+    }
+
+
+    return 0;
+
+}
