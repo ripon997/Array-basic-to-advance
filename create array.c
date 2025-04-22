d031dd2d4da67644d741a8e636117bf39c137bb0
Ripon <ripon365cse@gmail.com>
2025-04-22 21:17:16 +0600

    create array

---
+++ create array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int numbers[5];
+
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+    printf("%d\n",numbers[0]);
+    printf("%d\n",numbers[1]);
+    printf("%d\n",numbers[2]);
+    printf("%d\n",numbers[3]);
+    printf("%d\n",numbers[4]);
+
+
+    return 0;
+
+}
